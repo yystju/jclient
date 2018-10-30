def66c17532df9692bc2ce02b889ce82aa482a1e
shiquan <shiquan>
2018-10-30 11:54:23 +0800

    ...

--- src/main/java/MySocket/m_intPstr.java
+++
@@ -1,9 +0,0 @@
-package MySocket;
-
-public class m_intPstr {
-	int intCount=0;
-	String strCount=new String();
-	String strCount1=new String();
-	String strCount2=new String();
-	String strCount3=new String();
-}
--- src/main/java/SQLserver/m_SQLserver.java
+++
@@ -1,8 +0,0 @@
-package SQLserver;
-
-public class m_SQLserver {
-	public String IP=new String("10.130.1.15");
-	public String DbName=new String("SMES");
-	public String OP=new String("root");
-	public String PW=new String("led-123@");
-}
